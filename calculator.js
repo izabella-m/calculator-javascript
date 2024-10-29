24bcec2fce0888c814a1c9843e203c53200b0909
Izabella Maria <izabellamaria@MacBook-Air-de-Izabella.local>
2024-10-29 15:11:00 -0300

    intern project

--- calculator.js
+++ calculator.js
@@ -4,18 +4,17 @@ const buttons = document.querySelectorAll('button');
 /* Armazena valores */
 let selectedButtonNumber = '';
 let selectedButtontOperator = '';
-let previousNumber = '';
+let previousNumber = ''; // numero anterior ao operador
 let displayResult = '';
 
 /* Funcionalidades dos valores */
 
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
+buttons.forEach(button => { 
+  button.addEventListener('click', () => { 
     const value = button.textContent;
-    const action = button.dataset.action;
+    const action = button.dataset.action; 
 
-    if 
-    (action === 'delete') {
+    if (action === 'delete') {
       selectedButtonNumber = selectedButtonNumber.slice(0, -1);
       display.value = selectedButtonNumber;
     } else if (value === 'c') {
@@ -24,12 +23,12 @@ buttons.forEach(button => {
       previousNumber = '';
       display.value = '';
     } else if (value === '+' || value === '-' || value === '*' || value === '/') {
-      selectedButtontOperator = value;
-      previousNumber = selectedButtonNumber;
-      selectedButtonNumber = '';
+      selectedButtontOperator = value; 
+      previousNumber = selectedButtonNumber; // numero atual armazenado na variavel
+      selectedButtonNumber = ''; // receber o próximo valor
     } else if (value === '=') {
       let result;
-      switch (selectedButtontOperator) {
+      switch (selectedButtontOperator) { // bloco operador selecionado
         case '+':
           result = parseFloat(previousNumber) + parseFloat(selectedButtonNumber);
           break;
@@ -46,21 +45,21 @@ buttons.forEach(button => {
             result = parseFloat(previousNumber) / parseFloat(selectedButtonNumber);
           }
           break;
-        default:
+        default: // selectedButtonOperator inválido retorna e sai
           return;
       }
       selectedButtonNumber = result.toString();
-      display.value = selectedButtonNumber;
-      selectedButtontOperator = '';
+      display.value = selectedButtonNumber; // após o resultado
+      selectedButtontOperator = ''; // prepara para a próxima operação
       previousNumber = '';
       displayResult = true;
     } else {
-      if (displayResult && !isNaN(value)) { 
+      if (displayResult && !isNaN(value)) { // se o resultado tá sendo exibido e um número foi clicado
         selectedButtonNumber = value;
         displayResult = false; // Reseta o estado
       } else {
         if (value === '.' && selectedButtonNumber === '') {
-          selectedButtonNumber = '0.'; // ao digitar '.' direto,adiciona zero antes
+          selectedButtonNumber = '0.';
         } else {
           selectedButtonNumber += value;
         }
